84c6b18b522e063df4db2847f903afac8710d2ff
Jack Ma <mapeiyao@gmail.com>
2020-05-26 11:47:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,7 @@
 class Dog
-  def initialize(name="Fido", breed="Mutt")
-    @name = name, @breed = breed
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
   end
  
   def name
