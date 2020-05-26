a61b33c780079e4cfa00d616eb80409e2cc09f00
Jack Ma <mapeiyao@gmail.com>
2020-05-26 11:47:30 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+ 
+  def name
+    @name = name, @breed = breed
+  end
+ 
+  def name
+    @name
+  end
+  
+  def breed
+    @Mutt
+  end
+end
